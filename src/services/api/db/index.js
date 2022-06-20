5cd1baa95016ee8f12a2c7be817dbca676869f3f
Sulleimenov <sulleimenov@yandex.ru>
2022-06-20 18:10:33 +0600

    fix 3

--- src/services/api/db/index.js
+++ src/services/api/db/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const db = axios.create({
-  baseURL: "https://freelance-student-job.herokuapp.com/",
+  baseURL: "https://freelance-student-job.herokuapp.com",
 });
 
 export default db;
